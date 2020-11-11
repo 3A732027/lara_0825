47eaa7361d2f97ec9c2f3b513f3fe872f3955b6d
Pai Yen Chang <ken1100327@gmail.com>
2020-11-11 21:31:19 +0800

    modify web.php & PostsController.php 將show($id)註解

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -55,10 +55,10 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+//    public function show($id)
+//    {
+//        //
+//    }
 
     /**
      * Show the form for editing the specified resource.
